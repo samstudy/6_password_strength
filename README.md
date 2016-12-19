7b32ff630cdc4090103f9e036408b9f6d92e4153
sam156 <bskordo@gmail.com>
2016-12-19 00:09:34 -0500

    Task is done

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 6_password_strength
+###The script will estimate your password's strength
+*Run the script like -->**python password_strength.py**
+*After you should input your password to find out it's strenght
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,25 @@
-
+import re
 
 def get_password_strength(password):
-    pass
+    strength = 0
+    if re.search(r'\d', password):
+        strength += 1
+    if re.search(r'[A-Z]', password):
+        strength +=1
+    if re.search(r'[a-z]', password):
+        strength +=1
+    if len(password) > 6:
+        strength +=1
+    if len(password) > 12:
+        strength +=2
+    if re.search(r'[!#$%&():*+,-./[\\\]^_`{|}~]', password):
+        strength +=2
+    if re.search(r'[\w][\d][\w][\d][\w][\d]', password):
+        strength +=2
+    return strength
 
 
 if __name__ == '__main__':
-    pass
+    print('Now we will estimate your password\'s strength,1--weak... 10--strongest')
+    password = input('Please input your password:')
+    print('Your password\'s strength is',get_password_strength(password))
