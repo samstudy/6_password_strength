bcc5d4560834e8c487f56a56e47666b4bd85be2e
Samat Karymshakov <bskordo@gmail.com>
2017-01-05 20:14:46 +0600

    Hope it is my last attempt

--- password_strength.py
+++ password_strength.py
@@ -1,68 +1,26 @@
 import re
 
-
-
-def estimate_weight(password):
-    weight = ''.join(password)
-    count = len(password)
-    return count_weight(weight,count)
-
-
-def count_weight(weight,count):
-    if password.islower() or password.isupper():
-        if count == 3:
-            return 1
-        if count > 3:
-            return 2
-        else:
-            return 0
-    else:
-        if count == 1:
-            return 1
-        if count > 1:
-            return 2
-        else:
-           return 0
-
-        
-def check_password_uppercase(password):
-    upper_case = re.findall(r'[A-Z]',password)
-    return estimate_weight(upper_case)
-
-
-def check_password_lowercase(password):
-    lower_case = re.findall(r'[a-z]',password)
-    return estimate_weight(lower_case)     
-
-
-def check_password_digit(password):
-    digits = re.findall(r'[0-9]',password)
-    return estimate_weight(digits)
-
-
-def check_password_symbol(password):
-    symbol = re.findall(r'[!#$%&():*+,-./[\\\]^_`{|}~]',password)
-    return estimate_weight(symbol)
-
-
-def check_password_length(password):
-    if len(password) > 6:
-        return 1
-    if len(password) > 12:
-        return 2
-    else:
-        return 0
-
-
 def get_password_strenght(password):
-    count_lower_case = check_password_lowercase(password)
-    count_upper_case = check_password_uppercase(password)
-    count_length = check_password_length(password)
-    count_digits = check_password_digit(password)
-    count_symbols = check_password_symbol(password)
-    return  count_upper_case + count_lower_case + count_length + count_digits + count_symbols
-    
-    
+    strength = 0
+    if re.search(r'[0-9]',password):
+        strength += 1
+    if re.search(r'[A-Z]',password):
+        if len(password) > 3:
+            strength += 2
+        if len(password) == 2:
+            strength +=1
+    if re.search(r'[a-z]',password):
+        if len(password) > 3:
+            strength += 2
+        if len(password) == 2:
+            strength +=1
+    if len(password) > 8:
+        strength +=1
+    if len(password) > 12:
+        strength +=2
+    if re.search(r'[ !#$%&():*+,-./[\\\]^_`{|}~]', password):
+        strength +=2
+    return strength
         
 
 if __name__ == '__main__':
