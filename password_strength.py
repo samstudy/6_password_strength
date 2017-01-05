08575a9540c7b0d50ce36b2cd7f52b67f60687ad
Samat Karymshakov <bskordo@gmail.com>
2017-01-05 21:46:07 +0600

    Hope it is my last attempt

--- password_strength.py
+++ password_strength.py
@@ -2,26 +2,46 @@ import re
 
 def get_password_strenght(password):
     strength = 0
-    if re.search(r'[0-9]',password):
-        strength += 1
-    if re.search(r'[A-Z]',password):
-        if len(password) > 3:
-            strength += 2
-        if len(password) == 2:
-            strength +=1
-    if re.search(r'[a-z]',password):
-        if len(password) > 3:
-            strength += 2
-        if len(password) == 2:
-            strength +=1
+    if check_password_upper_case(password):
+        strength +=2
+    if check_password_lower_case(password):
+        strength +=1
+    if check_password_digit(password):
+        strength +=1
+    if check_password_symbol(password):
+        strength +=1
     if len(password) > 8:
         strength +=1
     if len(password) > 12:
         strength +=2
-    if re.search(r'[ !#$%&():*+,-./[\\\]^_`{|}~]', password):
+    if len(password) > 16:
         strength +=2
     return strength
-        
+
+
+
+
+def check_password_digit(password):
+    if re.search(r'[0-9]',password):
+        return True
+
+def check_password_upper_case(password):
+    if re.search(r'[A-Z]',password):
+        if len(password) >2:
+            return True
+        else:
+            return False
+
+def check_password_lower_case(password):
+    if re.search(r'[a-z]',password):
+        if len(password) >2:
+            return True
+        else:
+            return False
+def check_password_symbol(password):
+    if re.search(r'[!#$%&():*+,-./[\\\]^_`{|}~]',password):
+        return True
+
 
 if __name__ == '__main__':
     print('Now we will estimate your password\'s strength,1--weak... 10--strongest')
